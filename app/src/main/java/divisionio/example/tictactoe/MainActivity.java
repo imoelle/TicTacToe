cb06dd057bfd614a9ce3efa1306cef99aaccc6c4
Ingo <ingo.moeller@divisionio.de>
2021-03-23 08:16:42 +0100

    reset to game ready bug

--- app/src/main/java/divisionio/example/tictactoe/MainActivity.java
+++ app/src/main/java/divisionio/example/tictactoe/MainActivity.java
@@ -1,36 +1,39 @@
 package divisionio.example.tictactoe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    private static int[][] playGround = new int[3][3];
+    private static int[] playGround = new int[9];
     private static int player = 0;
+    private static boolean gameReady = true;
+    private TextView textGameState;
+    private Button startNewGame;
 
-    public void isClicked(View view) throws InterruptedException {
+    public void isClicked(View view){
         ImageView image = (ImageView) view;
-        int[] location = getLocationFromString(image);
+        int position = Integer.parseInt(image.getTag().toString());
 
-        if (locationIsEmpty(location[0], location[1])) {
+        if (locationIsEmpty(position) && gameReady) {
             if (player == 0) {
-                setGameIndex(location[0], location[1]);
+                playGround[position] = player;
                 player = 1;
             } else {
-                setGameIndex(location[0], location[1]);
+                playGround[position] = player;
                 player = 0;
             }
             chipAnimation(player, image);
             setPlayersTurn(player);
-        } else {
-            Toast.makeText(this, "This position is occupied", Toast.LENGTH_SHORT).show();
+            checkWinner(playGround);
         }
     }
 
@@ -39,58 +42,83 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textGameState = (TextView) findViewById(R.id.txtViewGameState);
+        startNewGame = (Button) findViewById(R.id.btnPlayAgain);
+
+        startNewGame.setText("Reset Game");
+
         initPlayGround();
-    }
-
-    // Tag Management
-    private int[] getLocationFromString(ImageView view) {
-        String[] locationString = view.getTag().toString().split(":");
-        return getCoordinates(locationString);
-    }
-
-    private int[] getCoordinates(String[] locationAsString) {
-        int[] locationAsInt = new int[locationAsString.length];
-
-        for (int i = 0; i < locationAsInt.length; i++)
-            locationAsInt[i] = Integer.parseInt(locationAsString[i]);
-
-        return locationAsInt;
     }
 
     // Game Management
     private void setPlayersTurn(int player) {
-        TextView playersTurn = (TextView) findViewById(R.id.txtViewGameState);
-        playersTurn.setText((player != 0) ? "It is player two's turn" : "It is player one's turn");
+        textGameState.setText((player != 0) ? "Player two's turn" : "Player one's turn");
     }
 
-    private boolean locationIsEmpty(int x, int y) {
-        return playGround[x][y] < 0;
+    private boolean locationIsEmpty(int pos) {
+        return playGround[pos] < 0;
     }
 
-    private void setGameIndex(int row, int column) {
-        playGround[row][column] = player;
+    private void checkWinner(int[] playGround) {
+        int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6},
+                {1, 4, 7}, {2, 5, 8}, {2, 4, 6}, {0, 4, 8}};
+
+        for (int[] winningPosition : winningPositions) {
+
+            if ((playGround[winningPosition[0]] == playGround[winningPosition[1]]) &&
+                    (playGround[winningPosition[1]] == playGround[winningPosition[2]]) &&
+                    (playGround[winningPosition[0]] != -1)) {
+
+                gameReady = false;
+                printWinner(player);
+                startNewGame.setText("Start new game");
+            } else {
+
+                gameReady = false;
+
+                for (int gameState : playGround) {
+                    if (gameState == -1)
+                        gameReady = true;
+                }
+                if (!gameReady) {
+                    textGameState.setText("Undecided game");
+                    startNewGame.setText("Start new game");
+                }
+            }
+        }
+    }
+
+    private void printWinner ( int player){
+        textGameState.setText((player == 1) ? "Player one (yellow) wins" : "Player two (red) wins");
+    }
+
+    public void buttonClicked (View view){
+        GridLayout gridLayout = findViewById(R.id.gridLayout);
+
+        for (int child = 0; child < gridLayout.getChildCount(); child++) {
+            ImageView children = (ImageView) gridLayout.getChildAt(child);
+            children.setImageDrawable(null);
+        }
+        initPlayGround();
+        player = 0;
+        gameReady = true;
+        startNewGame.setText("Reset game");
+        textGameState.setText("Player one's turn");
     }
 
     // Animation Methods
-    private void chipAnimation(int player, ImageView image) {
+    private void chipAnimation ( int player, ImageView image){
         image.setTranslationY(-1500);
         image.setImageResource((player == 0) ? R.drawable.redchip : R.drawable.yellowchip);
-        image.animate().translationYBy(1500).setDuration(300);
+        image.animate().translationYBy(1500).setDuration(350);
     }
 
     // Methods for Playground
-    private void initPlayGround() {
-        for (int row = 0; row < playGround.length; row++) {
-            Arrays.fill(playGround[row], -1);
-        }
+    private void initPlayGround () {
+        Arrays.fill(playGround, -1);
     }
 
-    public void logPlayGround(View view) {
-        for (int row = 0; row < playGround.length; row++) {
-            for (int col = 0; col < playGround[row].length; col++)
-                Log.i("PlayGround", "(" + String.valueOf(row) +
-                        ", " + String.valueOf(col) + ") = " +
-                        String.valueOf(playGround[row][col]));
-        }
+    public void logPlayGround (View view){
+        Log.i("playGround", Arrays.toString(playGround));
     }
 }
